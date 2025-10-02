af90e6be83612e4a3dfff8d51b377a18dd6501e9
phizzii <sophiebotten06@gmail.com>
2025-10-02 14:07:57 +0100

    add function for graphs

--- circlefirstfile.py
+++ circlefirstfile.py
@@ -1,1 +1,16 @@
+# importing relevant modules
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy
+import seaborn as sns
 
+integration_dataset_df = pd.read_csv("continuous_integration_data_pipeline_dataset.csv")
+
+fig, axes = plt.subplots(figsize=(12,8))
+plt.scatter((integration_dataset_df['X'])(integration_dataset_df['Y']), s=100, edgecolors='black')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.grid(True)
+plt.tight_layout
+plt.show()
