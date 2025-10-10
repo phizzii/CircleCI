39f591e5767fb93f0d41e256f913ad10df72b869
phizzii <sophiebotten06@gmail.com>
2025-10-10 12:07:28 +0100

    attempt fix for file not found error 2

--- unittest.py
+++ unittest.py
@@ -2,11 +2,11 @@ import pandas as pd
 import os
 
 def csv_saved_unit_test():
-    df = pd.read_csv("/Users/sophieb/Visual Studio Code/CircleCI/*.csv")
-    print(df)
-    #if 'output' in os.listdir('/Users/sophieb/Visual Studio Code/CircleCI'):
-     #   if len(os.listdir('/Users/sophieb/Visual Studio Code/CircleCI'))>0:
-      #      x=[_ for _ in os.listdir('/Users/sophieb/Visual Studio Code/CircleCI') if _[-3:]=='csv'][0]
-       #     new_file=pd.read_csv(x)
+    #df = pd.read_csv("/Users/sophieb/Visual Studio Code/CircleCI/*.csv")
+    #print(df)
+    if 'output' in os.listdir('/Users/sophieb/Visual Studio Code/CircleCI'):
+        if len(os.listdir('/Users/sophieb/Visual Studio Code/CircleCI'))>0:
+            x=[_ for _ in os.listdir('/Users/sophieb/Visual Studio Code/CircleCI') if _[-3:]=='csv'][0]
+            new_file=pd.read_csv(x)
 
 csv_saved_unit_test()
