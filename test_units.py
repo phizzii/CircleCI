73f9d93803446f4d5e36cccfc1e9e85b0cb936e3
phizzii <sophiebotten06@gmail.com>
2025-10-16 10:59:18 +0100

    new test function

--- test_units.py
+++ test_units.py
@@ -17,3 +17,18 @@ def test_csv_saved():
     df = pd.read_csv(csv_path)
 
     assert not df.empty, f"csv file '{csv_files[0]}' is empty."
+
+def test_csv_values():
+    folder_path = os.getcwd() # Ensure this path exists
+    if not os.path.exists(folder_path):
+        raise FileNotFoundError(f"The specified directory does not exist: {folder_path}")
+
+    csv_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
+    assert csv_files, "no csv files in folder"
+
+    csv_path = os.path.join(folder_path, csv_files[0])
+    df = pd.read_csv(csv_path)
+
+    numeric_df = df.apply(pd.to_numeric, errors='coerce')
+    is_all_numeric = not numeric_df.isnull().values.any()
+    print(is_all_numeric)
