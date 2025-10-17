bc3c098ed16db94d1c525e79c35ca952685434e5
phizzii <sophiebotten06@gmail.com>
2025-10-17 10:12:22 +0100

    adding new test to make sure the correlation is higher than 0.95

--- test_units.py
+++ test_units.py
@@ -6,7 +6,8 @@ import os
 print(os.getcwd())
 
 def test_csv_saved():
-    folder_path = os.getcwd() # Ensure this path exists
+    # testing to make sure the csv file actually exists to be analysed from
+    folder_path = os.getcwd()
     if not os.path.exists(folder_path):
         raise FileNotFoundError(f"The specified directory does not exist: {folder_path}")
 
@@ -19,7 +20,25 @@ def test_csv_saved():
     assert not df.empty, f"csv file '{csv_files[0]}' is empty."
 
 def test_csv_values():
-    folder_path = os.getcwd() # Ensure this path exists
+    # testing to make sure all values in the csv file are numerical and do not contain null values
+    folder_path = os.getcwd()
+    if not os.path.exists(folder_path):
+        raise FileNotFoundError(f"The specified directory does not exist: {folder_path}")
+
+    csv_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
+    assert not df.empty, f"csv file '{csv_files[0]}' is empty."
+
+    csv_path = os.path.join(folder_path, csv_files[0])
+    df = pd.read_csv(csv_path)
+
+    numeric_df = df.apply(pd.to_numeric, errors='coerce')
+    is_all_numeric = not numeric_df.isnull().values.any()
+    print(is_all_numeric)
+
+    assert not numeric_df.isnull().values.any(), f"csv contains null/non-numerical values"
+
+def test_correlation_coefficient():
+    folder_path = os.getcwd()
     if not os.path.exists(folder_path):
         raise FileNotFoundError(f"The specified directory does not exist: {folder_path}")
 
@@ -29,6 +48,9 @@ def test_csv_values():
     csv_path = os.path.join(folder_path, csv_files[0])
     df = pd.read_csv(csv_path)
 
-    numeric_df = df.apply(pd.to_numeric, errors='coerce')
-    is_all_numeric = not numeric_df.isnull().values.any()
-    print(is_all_numeric)
+    df.corr()
+
+    if df.corr() < 0.95:
+        raise TypeError(f"file contains non-numerical/null values")
+    
+            
